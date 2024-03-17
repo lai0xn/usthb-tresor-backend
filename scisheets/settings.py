9aa79e23e95b36934bd527c52a7b21cf30a718d3
lai0xn <onigenko12@gmail.com>
2024-03-17 01:06:07 +0100

    fixed missing comma

--- scisheets/settings.py
+++ scisheets/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
